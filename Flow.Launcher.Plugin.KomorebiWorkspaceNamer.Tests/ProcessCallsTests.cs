e5e09d976d5c6df6617cf903972d5fd60525c0f8
davor-skontra <thekrad23@gmail.com>
2025-05-02 13:44:44 +0300

    Add a test fro getting config json

--- Flow.Launcher.Plugin.KomorebiWorkspaceNamer.Tests/ProcessCallsTests.cs
+++ Flow.Launcher.Plugin.KomorebiWorkspaceNamer.Tests/ProcessCallsTests.cs
@@ -1,6 +1,12 @@
 ﻿namespace Flow.Launcher.Plugin.KomorebiWorkspaceNamer.Tests;
 
-public class ProcessCallsTest
+public class ProcessCallsTests
 {
-    []
+    [Test]
+    public void ShouldFindConfigFile()
+    {
+        var result = ProcessCalls.GetKomorebiConfigJson();
+        
+        Assert.That(result, Is.Not.Empty);
+    }
 }
