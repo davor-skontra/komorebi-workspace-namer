306145803924b72f4f7e02afb2ad998ec19f2579
davor-skontra <thekrad23@gmail.com>
2025-05-17 12:58:44 +0300

    Add a style change delegate

--- Flow.Launcher.Plugin.KomorebiWorkspaceNamer/UserConfig/Settings.cs
+++ Flow.Launcher.Plugin.KomorebiWorkspaceNamer/UserConfig/Settings.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Flow.Launcher.Plugin.KomorebiWorkspaceNamer.UserConfig;
 
 public class Settings
 {
     private const char PresetNamesSeparator = ',';
-    public IndexStyler.Kind IndexStyler { get; set; } = Plugin.KomorebiWorkspaceNamer.IndexStyler.Kind.ArabicNumerals;
+
+    public delegate void StyleChangedDelegate(IndexStyler.Kind previous, IndexStyler.Kind current);
+
+    public event StyleChangedDelegate? StyleChangeEvent;
+    
+    private IndexStyler.Kind _indexStyler = Plugin.KomorebiWorkspaceNamer.IndexStyler.Kind.ArabicNumerals;
+    public IndexStyler.Kind IndexStyler
+    {
+        get => _indexStyler;
+        set
+        {
+            var previous = _indexStyler;
+            _indexStyler = value;
+            StyleChangeEvent?.Invoke(previous, _indexStyler);
+        }
+    }
+
     public string PresetNames { get; set; } = "";
 
     public IEnumerable<string> GetPresetNames()
